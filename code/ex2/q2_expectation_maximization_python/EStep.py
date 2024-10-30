072c7cae0250f02fba874e1e8f45fc99e9772cee
David Niebert <david.niebert@googlemail.com>
2024-10-30 17:05:11 +0100

    Start implementing helpers.py

--- code/ex2/q2_expectation_maximization_python/EStep.py
+++ code/ex2/q2_expectation_maximization_python/EStep.py
@@ -20,4 +20,31 @@ def EStep(means, covariances, weights, X):
     # gamma          : NxK matrix of responsibilities for N datapoints and K Gaussians.
 
     #####Insert your code here for subtask 6b#####
+
+    def prob_gaussian(x, mu, variance):
+        normalizer = 1 / (np.sqrt(2 * np.pi) * (variance)**2)
+        exponent = np.exp(-1 * ((x - mu)**2) / (2 * (variance ** 2)))
+        return normalizer * exponent
+
+    logLikelihood = getLogLikelihood(means, weights, covariances, X)
+
+    N, _ = X.shape
+    K = len(weights)
+    gamma = np.zeros(N, K)
+    variances = np.array([np.diagonal(covariances[:, :, k]) for k in range(K)])
+
+    for x in range(N):
+        sum_posterior_gaussians = 0
+        for k in range(K):
+            prior_k = weights[k]
+            prob_k = prob_gaussian(x, means[k], variances[k])
+            sum_posterior_gaussians += prior_k * prob_k
+
+        for j in range(K):
+            # j is current Gaussian
+            prior_j = weights[j]
+            prob_j = prob_gaussian(x, means[j], variances[j])
+            y_j_x_n = (prior_j, prob_j) / sum_posterior_gaussians
+            gamma[x][j] = y_j_x_n
+
     return [logLikelihood, gamma]
